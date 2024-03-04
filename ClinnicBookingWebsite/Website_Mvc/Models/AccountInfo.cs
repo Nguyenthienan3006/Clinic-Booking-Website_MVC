fbf4559e02cfe5b74a0890a98a736496dd1cdadc
Nguyenthienan3006 <92444045+Nguyenthienan3006@users.noreply.github.com>
2024-03-04 15:47:34 +0700

    update 4-3

--- ClinnicBookingWebsite/Website_Mvc/Models/AccountInfo.cs
+++ ClinnicBookingWebsite/Website_Mvc/Models/AccountInfo.cs
@@ -6,6 +6,10 @@ namespace Website_Mvc.Models
     public partial class AccountInfo
     {
         public int IdUser { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string? BloodGroup { get; set; }
         public string? Phonenumber { get; set; }
         public string? Address { get; set; }
         public string? Email { get; set; }
