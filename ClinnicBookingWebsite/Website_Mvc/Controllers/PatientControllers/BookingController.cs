5ed9cb72ec6b4f9da96e21bb6745dc35d093e383
Nguyenthienan3006 <92444045+Nguyenthienan3006@users.noreply.github.com>
2024-03-06 22:49:15 +0700

    add booking function 1

---
+++ ClinnicBookingWebsite/Website_Mvc/Controllers/PatientControllers/BookingController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Website_Mvc.Controllers.PatientControllers
+{
+    public class BookingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CheckOut(string datetime)
+        {
+            return View();
+        }
+    }
+}
